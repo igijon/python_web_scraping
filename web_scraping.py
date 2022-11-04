6ca1513505efc938576ed2a07cbfe100010fcc76
Inma Gijón <igijoninf@gmail.com>
2022-11-04 17:55:07 +0100

    Extraer título de la página. Ejemplo enlaces.

---
+++ web_scraping.py
@@ -0,0 +1,12 @@
+import bs4
+import requests
+
+url = 'https://www.filmaffinity.com/es/ranking.php?rn=ranking_scifi'
+result = requests.get(url)
+print(type(result), result.text)
+soup = bs4.BeautifulSoup(result.text, 'lxml')
+print(soup)
+print(soup.select('title'))
+print(soup.select('title')[0].getText())
+links = soup.select('a')
+print(links[0].getText())
