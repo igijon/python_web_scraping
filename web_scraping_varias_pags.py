a18a4044379064ac5546ed94737fb9425ce03946
Inma Gijón <igijoninf@gmail.com>
2022-11-05 10:48:52 +0100

    Extraigo los títulos de los libros con 4 o más estrellas.

--- web_scraping_varias_pags.py
+++ web_scraping_varias_pags.py
@@ -1,10 +1,22 @@
 import bs4
 import requests
 
-page = 1
-url = 'http://books.toscrape.com/catalogue/page-{}.html'
 
-while requests.get(url.format(page)).ok:
-    print(url.format(page))
-    page += 1
+def get_books_with_stars():
+    page = 1
+    url = 'http://books.toscrape.com/catalogue/page-{}.html'
+    result = requests.get(url.format(page))
+    while result.ok:
+        get_books_per_page(result)
+        page += 1
+        result = requests.get(url.format(page))
 
+def get_books_per_page(result):
+    soup = bs4.BeautifulSoup(result.text, 'lxml')
+    books = soup.select('.product_pod')
+    for b in books:
+        if b.select('.star-rating.Four') or b.select('.star-rating.Five'):
+            print(b.select('h3 a')[0]['title'])
+
+
+get_books_with_stars()
