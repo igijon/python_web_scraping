2544cb88d05e4b9accaddfdb5ad67b42f8d33b36
Inma Gijón <igijoninf@gmail.com>
2022-11-05 09:16:57 +0100

    Extraer elementos de una clase.

--- web_scraping.py
+++ web_scraping.py
@@ -10,3 +10,9 @@ print(soup.select('title'))
 print(soup.select('title')[0].getText())
 links = soup.select('a')
 print(links[0].getText())
+
+movies = soup.select('.movie-card')
+print(len(movies))
+movies = soup.select('.movie-card .mc-poster a')
+for m in movies:
+    print(m.attrs['href'])
