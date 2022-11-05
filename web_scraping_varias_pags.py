ee99834d42c1d9facc11d61c6713d533c1ed0187
Inma Gijón <igijoninf@gmail.com>
2022-11-05 10:59:36 +0100

    Mejoras

--- web_scraping_varias_pags.py
+++ web_scraping_varias_pags.py
@@ -6,17 +6,23 @@ def get_books_with_stars():
     page = 1
     url = 'http://books.toscrape.com/catalogue/page-{}.html'
     result = requests.get(url.format(page))
+    all_books = []
     while result.ok:
-        get_books_per_page(result)
+        all_books+=get_books_per_page(result)
         page += 1
         result = requests.get(url.format(page))
+    return all_books
 
 def get_books_per_page(result):
     soup = bs4.BeautifulSoup(result.text, 'lxml')
     books = soup.select('.product_pod')
+    books_result_page = []
     for b in books:
         if b.select('.star-rating.Four') or b.select('.star-rating.Five'):
+            books_result_page.append(b.select('h3 a')[0]['title'])
             print(b.select('h3 a')[0]['title'])
 
+    return books_result_page
 
-get_books_with_stars()
+
+print(len(get_books_with_stars()))
